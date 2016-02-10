5b6abcbfec1dc72892c87e7f4d200ce0527fa7cc
Michael <mcpower@users.noreply.github.com>
2016-02-10 19:56:19 +1100

    Add ability to store login details to disk

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const menubar = require('menubar');
 const path = require('path');
 const electron = require("electron");
@@ -7,15 +8,38 @@ const app = electron.app;
 const ipcMain = electron.ipcMain;
 const api_url = "https://allocate.timetable.monash.edu/aplus-2016/rest/student/"
 const homepage_url = "https://allocate.timetable.monash.edu/aplus-2016/student/"
+const cred_store_file = "savedcreds.json";
 var request = require("request").defaults({jar: true});
 var data;
 
-ipcMain.on("async-login", function(event, username, password) {
-	request.post({url: api_url +"login", form: {username: username, password: password}}, function (_, _, login){
+var savedCreds = {remember: false};
+
+fs.stat(cred_store_file, function (err, stats) {
+	if (!err && stats.isFile()) {
+		savedCreds = JSON.parse(fs.readFileSync(cred_store_file));
+	}
+});
+
+ipcMain.on("async-login", function(event, username, password, remember) {
+	request.post({url: api_url + "login", form: {username: username, password: password}}, function (_, _, login){
 		var login = JSON.parse(login);
 		if (!login.success) {
 			event.sender.send("login-reply", false);
 		} else {
+			if (remember) {
+				savedCreds = {
+					username: username,
+					password: password,
+					remember: remember
+				};
+				fs.writeFileSync(cred_store_file, JSON.stringify(savedCreds));
+			} else {
+				fs.stat(cred_store_file, function (err, stats) {
+					if (!err && stats.isFile()) {
+						fs.unlinkSync(cred_store_file);
+					}
+				});
+			}
 			request = request.defaults({qs: {ss: login.token}});
 			request(homepage_url, function (_, _, homepage){
 				data = JSON.parse(homepage.match(/^data=([^;]+);$/m)[1]);
@@ -28,6 +52,10 @@ ipcMain.on("async-login", function(event, username, password) {
 ipcMain.on("async-get-data", function (event) {
 	event.sender.send("get-data-reply", data);
 })
+
+ipcMain.on("async-get-savedcreds", function (event) {
+	event.sender.send("get-savedcreds-reply", savedCreds);
+});
 
 var mb = menubar({
 	skipTaskbar: true,
